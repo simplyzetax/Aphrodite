61bb84dc9b2d254c3a82663d4c899f194129488d
Zetax <ernzerhofffinn@gmail.com>
2024-04-05 05:27:14 +0200

    In game skin (mcp) support

---
+++ src/routes/mcp/dedicatedserver.ts
@@ -0,0 +1,43 @@
+import app, { db } from "../..";
+import { getACIDFromJWT } from "../../utils/auth";
+import { Aphrodite } from "../../utils/error";
+import { ProfileHelper } from "../../utils/builders/profile";
+import UAParser from "../../utils/version";
+import { and, eq, sql } from "drizzle-orm";
+import { profiles } from "../../database/models/profiles";
+import { bumpRvnNumber } from "./queryprofile";
+
+//TODO: Add rate limiting to prevent abuse
+app.post('/fortnite/api/game/v2/profile/:accountId/dedicated_server/:operation', async (c) => {
+
+    const unsafeAccountId = c.req.param("accountId");
+
+    const requestedProfileId = c.req.query("profileId");
+    if (!requestedProfileId) return c.sendError(Aphrodite.mcp.invalidPayload);
+
+    const ph = new ProfileHelper(requestedProfileId, 1);
+
+    const fullProfile = await ph.getProfile(unsafeAccountId);
+    if (!fullProfile) return c.sendError(Aphrodite.mcp.templateNotFound);
+
+    Promise.all([
+        bumpRvnNumber.execute({ unsafeAccountId, type: "athena" }),
+    ])
+
+    return c.json({
+        profileRevision: fullProfile.profile.rvn + 1,
+        profileId: fullProfile.profile.profileId,
+        profileChangesBaseRevision: fullProfile.profile.rvn,
+        profileChanges: [
+            {
+                changeType: "fullProfileUpdate",
+                profile: fullProfile.profile,
+            }
+        ],
+        profileCommandRevision: fullProfile.profile.rvn + 1,
+        serverTime: new Date().toISOString(),
+        multiUpdate: [],
+        responseVersion: 1,
+    })
+
+});
