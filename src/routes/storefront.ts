bd377b364d5ca4e6e768bfe545a49bebc9234131
itztiva <98614842+itztiva@users.noreply.github.com>
2024-04-07 00:40:16 -0400

    catalog

--- src/routes/storefront.ts
+++ src/routes/storefront.ts
@@ -4,6 +4,13 @@ import app from "..";
 
 const keychain = await Bun.file(path.join(import.meta.dir, "../../static/keychain.json")).json();
 
+const catalog = await Bun.file(path.join(import.meta.dir, "../../static/catalog.json")).json();
+
+
 app.get("/fortnite/api/storefront/v2/keychain", (c) => {
     return c.json(keychain);
+});
+
+app.get("/fortnite/api/storefront/v2/catalog", (c) => {
+    return c.json(catalog);
 });
